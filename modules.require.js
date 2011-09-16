a5a88f9a4e5784170dbcbbd475edc6441b91fd52
Andy VanWagoner <thetalecrafter@gmail.com>
2011-09-15 21:56:33 -0600

    All modules now share the same require function. No binding necessary.

--- modules.require.js
+++ modules.require.js
@@ -1,35 +1,39 @@
 var require = (function(){
-	var last = /\w+$/, ext = '.js', modules = {}, defs = {},
+	var last = /\w+$/, ext = '.js', modules = {}, defs = {}, stack = [],
 		readOnly = { writable:false, configurable:false, enumerable:true },
 		props = { id:readOnly, require:readOnly,
 			module:{ writable:false, configurable:false, enumerable:false } };
 
-	function require(name) { // name is a "top-level" module id
-		if (name.slice(-ext.length) === ext) { name = name.slice(0, -ext.length); }
-		if (!defs[name]) { throw new Error('"'+name+'" could not be loaded'); }
-		if (!modules[name]) {
-			var module = (modules[name] = {
-				exports:{}, id:name, require:function(id) {
-					// resolve relative module id
-					if (id.charAt(0) === '.') { id = name.replace(last, id); }
-					var orig = id.split('/'), terms = [], i, l = orig.length;
-					for (i = 0; i < l; ++i) {
-						if (orig[i] === '..') { terms.pop(); }
-						else if (orig[i] !== '.') { terms[terms.length] = orig[i]; }
-					}
-					return require(terms.join('/'));
-				}
-			});
-			module.module = module;
-			// read-only props, and cannot add or remove props if possible
-			if (Object.defineProperties) { Object.defineProperties(module, props); }
-			if (Object.seal) { Object.seal(module); }
-			defs[name].call(module);
+	function resolve(base, id) {
+		if (id.charAt(0) === '.') { id = base.replace(last, id); }
+		var orig = id.split('/'), terms = [], i, l = orig.length;
+		for (i = 0; i < l; ++i) {
+			if (orig[i] === '..') { terms.pop(); }
+			else if (orig[i] !== '.') { terms[terms.length] = orig[i]; }
 		}
-		return modules[name].exports;
+		return terms.join('/');
 	}
 
-	require.define = function(name, fn) { defs[name] = fn; };
+	function require(id) {
+		if (id.slice(-ext.length) === ext) { id = id.slice(0, -ext.length); }
+		if (stack.length) { id = resolve(stack[stack.length-1], id); }
+		if (!defs[id]) { throw new Error('Module "'+id+'" was not found.'); }
+		if (!modules[id]) {
+			var module = (modules[id] = { exports:{}, id:id, require:require });
+			module.module = module;
 
+			if (Object.defineProperties) { Object.defineProperties(module, props); }
+			if (Object.seal) { Object.seal(module); }
+
+			stack[stack.length] = id;
+			defs[id].call(module);
+			--stack.length;
+		}
+		return modules[id].exports;
+	}
+
+	require.define = function(id, fn) { defs[id] = fn; };
+
+	if (Object.defineProperties) { Object.defineProperties(require, { define:readOnly }); }
 	return require;
 })();
