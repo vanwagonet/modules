a0e70a99f57d9fb22c2fb2f421f29c63396de2a2
Andy VanWagoner <thetalecrafter@gmail.com>
2011-10-03 11:41:03 -0600

    made module.require work as it does in nodejs. Code cleanup.

--- modules.require.js
+++ modules.require.js
@@ -1,26 +1,14 @@
-var require = (function(){
-	var last = /\w+$/, ext = '.js', modules = {}, defs = {}, stack = [],
-		global = (window.global = window),
+(function(){
+	var global = (this.global = this), last = /\w+$/, ext = '.js',
+		modules = {}, defs = {}, stack = [],
 		readOnly = { writable:false, configurable:false, enumerable:true },
-		props = { id:readOnly/*, require:readOnly*/ };
-
-	function resolve(id) {
-		if (!stack.length) { return id; }
-		if (id.charAt(0) === '.') { id = stack[stack.length-1].replace(last, id); }
-		var orig = id.split('/'), terms = [], i, l = orig.length;
-		for (i = 0; i < l; ++i) {
-			if (orig[i] === '..') { terms.pop(); }
-			else if (orig[i] !== '.') { terms[terms.length] = orig[i]; }
-		}
-		return terms.join('/');
-	}
+		props = { id:readOnly, require:readOnly };
 
 	function require(oid) {
-		id = resolve(oid);
-		if (id.slice(-ext.length) === ext) { id = id.slice(0, -ext.length); }
+		var id = resolve(oid);
 		if (!defs[id]) { throw new Error('Module "'+oid+'" was not found.'); }
 		if (!modules[id]) {
-			var module = (modules[id] = { exports:{}, id:id/*, require:require*/ });
+			var module = (modules[id] = { exports:{}, id:id, require:require });
 			if (Object.defineProperties) { Object.defineProperties(module, props); }
 			if (Object.seal) { Object.seal(module); }
 
@@ -29,12 +17,23 @@ var require = (function(){
 			--stack.length;
 		}
 		return modules[id].exports;
-	}
+	} global.require = require;
+
+	function resolve(id) {
+		if (id.slice(-ext.length) === ext) { id = id.slice(0, -ext.length); }
+		var base = (this != global && this.id) || (stack.length && stack[stack.length-1]);
+		if (!base) { return id; }
+		if (id.charAt(0) === '.') { id = base.replace(last, id); }
+		var orig = id.split('/'), terms = [], i, l = orig.length;
+		for (i = 0; i < l; ++i) {
+			if (orig[i] === '..') { terms.pop(); }
+			else if (orig[i] !== '.') { terms[terms.length] = orig[i]; }
+		}
+		return terms.join('/');
+	} require.resolve = resolve;
 
 	require.define = function(id, fn) { defs[id] = fn; };
-	require.resolve = resolve;
 	require.cache = modules;
 
-	if (Object.defineProperties) { Object.defineProperties(require, { define:readOnly }); }
-	return require;
+	if (Object.defineProperties) { Object.defineProperties(require, { define:readOnly, resolve:readOnly, cache:readOnly }); }
 })();
