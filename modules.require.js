656dbdb8637ef8c279a5766f7d7268ab721d2344
Andy VanWagoner <thetalecrafter@gmail.com>
2011-10-02 21:56:10 -0600

    avoid with for lexical scope optimizations. add require.resolve and require.cache following nodejs.

--- modules.require.js
+++ modules.require.js
@@ -1,11 +1,12 @@
 var require = (function(){
 	var last = /\w+$/, ext = '.js', modules = {}, defs = {}, stack = [],
+		global = (window.global = window),
 		readOnly = { writable:false, configurable:false, enumerable:true },
-		props = { id:readOnly, require:readOnly,
-			module:{ writable:false, configurable:false, enumerable:false } };
+		props = { id:readOnly/*, require:readOnly*/ };
 
-	function resolve(base, id) {
-		if (id.charAt(0) === '.') { id = base.replace(last, id); }
+	function resolve(id) {
+		if (!stack.length) { return id; }
+		if (id.charAt(0) === '.') { id = stack[stack.length-1].replace(last, id); }
 		var orig = id.split('/'), terms = [], i, l = orig.length;
 		for (i = 0; i < l; ++i) {
 			if (orig[i] === '..') { terms.pop(); }
@@ -14,25 +15,25 @@ var require = (function(){
 		return terms.join('/');
 	}
 
-	function require(id) {
+	function require(oid) {
+		id = resolve(oid);
 		if (id.slice(-ext.length) === ext) { id = id.slice(0, -ext.length); }
-		if (stack.length) { id = resolve(stack[stack.length-1], id); }
-		if (!defs[id]) { throw new Error('Module "'+id+'" was not found.'); }
+		if (!defs[id]) { throw new Error('Module "'+oid+'" was not found.'); }
 		if (!modules[id]) {
-			var module = (modules[id] = { exports:{}, id:id, require:require });
-			module.module = module;
-
+			var module = (modules[id] = { exports:{}, id:id/*, require:require*/ });
 			if (Object.defineProperties) { Object.defineProperties(module, props); }
 			if (Object.seal) { Object.seal(module); }
 
 			stack[stack.length] = id;
-			defs[id].call(module);
+			defs[id].call(global, module, module.exports);
 			--stack.length;
 		}
 		return modules[id].exports;
 	}
 
 	require.define = function(id, fn) { defs[id] = fn; };
+	require.resolve = resolve;
+	require.cache = modules;
 
 	if (Object.defineProperties) { Object.defineProperties(require, { define:readOnly }); }
 	return require;
